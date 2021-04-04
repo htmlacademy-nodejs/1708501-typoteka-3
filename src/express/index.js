4710d32c2276b1f2bff25b862c432173f9d4466d
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-04-05 01:43:24 +0300

    feat: добавил набор статичных pug-шаблонов

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,23 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 
-const offersRoutes = require(`./routes/offers-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
-app.use(`/offers`, offersRoutes);
-app.use(`/my`, myRoutes);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
 
 app.listen(DEFAULT_PORT);
---
+++ src/express/routes/articles-routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const {Router} = require(`express`);
+const articlesRouter = new Router();
+
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
+articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`new-post`)); // TMP
+articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
+
+module.exports = articlesRouter;
