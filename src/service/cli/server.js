3bfd8e40e264b79f0e0edcdbdcc6e3007c9268bf
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-10-02 15:00:24 +0300

    feat: добавил логгер в мидлвары express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -12,9 +12,25 @@ const logger = getLogger({name: `api`});
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, apiRoutes);
 
-app.use((req, res) => res.status(HttpCode.NOT_FOUND).send(`Not found`));
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
