5b4c550ba1ba6405f78dd15a95f00351794cc2a0
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-10-02 16:07:56 +0300

    fix: some prettier fixes

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-
 
 module.exports.getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -13,7 +12,10 @@ module.exports.getRandomInt = (min, max) => {
 module.exports.shuffle = (someArray) => {
   for (let i = someArray.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
-    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+    [someArray[i], someArray[randomPosition]] = [
+      someArray[randomPosition],
+      someArray[i],
+    ];
   }
 
   return someArray;
