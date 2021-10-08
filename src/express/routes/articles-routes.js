23bf63c44b77eebfeb0704a619acdc7e5390cf1a
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-10-08 22:23:14 +0300

    feat: передал данные в шаблоны

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,10 +3,16 @@
 
 const {Router} = require(`express`);
 const articlesRouter = new Router();
+const api = require(`../api`).getAPI();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`new-post`)); // TMP
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([api.getArticle(id), api.getCategories()]);
+  res.render(`article/edit-article`, {article, categories});
+});
 articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
 
 module.exports = articlesRouter;
