3ea37530647d7a2d7222cb7f3eb64a8735c80c34
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-11-30 00:18:30 +0300

    fix: убрал лишние коментарии

--- src/service/models/category.js
+++ src/service/models/category.js
@@ -4,11 +4,6 @@ const {DataTypes, Model} = require(`sequelize`);
 
 class Category extends Model {}
 
-// CREATE TABLE categories (
-//   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//   name varchar(255) NOT NULL
-// );
-
 const define = (sequelize) =>
   Category.init(
       {
