98e575725f88553958a41a935851d0b81158925e
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-12-06 23:45:27 +0300

    feat: аутентификация, фронт и тесты для неё

--- src/express/index.js
+++ src/express/index.js
@@ -2,7 +2,10 @@
 
 const path = require(`path`);
 const express = require(`express`);
+const session = require(`express-session`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
 
+const sequelize = require(`../service/lib/sequelize`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
@@ -11,7 +14,30 @@ const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const UPLOAD_DIR = `upload`;
 
+const {SESSION_SECRET} = process.env;
+if (!SESSION_SECRET) {
+  throw new Error(`SESSION_SECRET environment variable is not defined`);
+}
+
 const app = express();
+
+const mySessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 180000,
+  checkExpirationInterval: 60000
+});
+
+sequelize.sync({force: false});
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: mySessionStore,
+  resave: false,
+  proxy: true,
+  saveUninitialized: false,
+}));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
