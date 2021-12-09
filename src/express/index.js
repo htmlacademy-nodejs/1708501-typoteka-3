ac4250a68c2fdbf424b2c9c4349f47cd128cb19e
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-12-09 18:11:21 +0300

    feat: поправил страницы ошибок

--- src/express/index.js
+++ src/express/index.js
@@ -9,6 +9,7 @@ const sequelize = require(`../service/lib/sequelize`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const {HttpCode} = require(`../service/constants`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
@@ -39,9 +40,6 @@ app.use(session({
   saveUninitialized: false,
 }));
 
-app.set(`views`, path.resolve(__dirname, `templates`));
-app.set(`view engine`, `pug`);
-
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
@@ -49,5 +47,13 @@ app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
+app.use((req, res) => res.status(HttpCode.NOT_FOUND).render(`errors/404`));
+
+app.use((err, _req, res, _next) => {
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+});
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
